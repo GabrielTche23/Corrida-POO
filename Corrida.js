91d001339d2af536a70fa5e1ad9299f8f2d40a25
Gabriel Rodrigo <pdktche-pb@live.com>
2022-09-20 20:06:17 -0300

    Foi criado uma classe e uma funçao para calcular a distancia percorrida em segundos.

---
+++ Corrida.js
@@ -0,0 +1,12 @@
+let resultado = 0
+
+class Carro{
+    Nome
+    Potencia
+    VelocidadeMax
+    Aceleracao
+
+    TempoEmSegundos(distanciaParametro){
+        return resultado = distanciaParametro / this.VelocidadeMax / this.Aceleracao
+    }
+}
